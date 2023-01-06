e8bf8ec329f4e0ba06646da53a22d6c2280327bb
aliuysal59 <ali.uysal@hotmail.com.tr>
2023-01-06 16:36:02 +0300

    Hipotenus

--- DikUcgen/DikUcgen/src/Main.java
+++ DikUcgen/DikUcgen/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        int a, b, c;
+        double d;
+
+        Scanner girisler = new Scanner(System.in);
+        System.out.print("1.Kenarı Giriniz: ");
+        a = girisler.nextInt();
+        System.out.print("2.Kenarı Giriniz: ");
+        b = girisler.nextInt();
+        System.out.print("3.Kenarı Giriniz: ");
+        c = girisler.nextInt();
+
+        d = (a+b+c)/2;
+        System.out.print("Ucgenin Alanı:" +d);
+
     }
 }
